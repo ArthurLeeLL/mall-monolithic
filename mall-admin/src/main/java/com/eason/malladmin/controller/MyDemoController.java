5d83709f548c0c7b439c96e6d92003c5ec34d37b
ArthurLeeLL <xingyuli1996@gmail.com>
2024-06-27 20:29:17 +0800

    Integrating swagger-ui

--- mall-admin/src/main/java/com/eason/malladmin/controller/MyDemoController.java
+++ mall-admin/src/main/java/com/eason/malladmin/controller/MyDemoController.java
@@ -1,5 +1,9 @@
 package com.eason.malladmin.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +15,17 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @Controller
+@Api(tags = "DemoController")
+@Tag(name = "DemoController", description = "demo")
 @RequestMapping("/demo")
 public class MyDemoController {
 
     @GetMapping("demoController")
+    @ApiOperation("demoApi")
     @ResponseBody
     public String DemoController(
-            @RequestParam int id
+            @RequestParam(value = "id", defaultValue = "-1")
+            @ApiParam("id") int id
     ) {
         return "This is a demo." + id;
     }
